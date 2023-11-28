9e70f3e8d807903e9e4ef6a5129b2ccc52a66c37
Annie Wilson <annemarie.wilson@yahoo.com>
2023-11-28 15:40:23 +0000

    completed part 1 of MVP

---
+++ index.js
@@ -0,0 +1,6 @@
+const getCountryByName = async (countryName) => {
+    const response = await fetch("https://restcountries.com/v3.1/name/" + countryName);
+    const data = await response.json();
+    console.log(data);
+}
+getCountryByName("cameroon");
