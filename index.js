d4796ac91cd78d062209a0ec50d49ddc2a59a6af
Annie Wilson <annemarie.wilson@yahoo.com>
2023-11-29 09:11:17 +0000

    finished extensions

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-const getCountryByName = async (countryName) => {
-    const response = await fetch("https://restcountries.com/v3.1/name/" + countryName);
-    const data = await response.json();
-    console.log(data);
+async function getAllCountries() {
+    try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        displayCountries(data);
+    } catch (error) {
+        displayError("Error fetching countries");
+    }
+}
 
+async function getCountryByName(countryName) {
+    try {
+        const response = await fetch("https://restcountries.com/v3.1/name/" + countryName);
+        const data = await response.json();
+        displayCountries(data);
+    } catch (error) {
+        displayError("Error fetching the country");
+    }
+}
 
-    data.forEach((country) => {
-        const countryContainer = document.createElement("div");
-
-       // Display country name
-       const nameCountry = document.createElement("h2");
-       nameCountry.innerText = `Country: ${country.name.common}`;
-       countryContainer.appendChild(nameCountry);
-
-        // Display capital
-        const countryCapital = document.createElement("p");
-        countryCapital.innerText = `Capital: ${country.capital}`;
-        countryContainer.appendChild(countryCapital);
-
-        // Display languages
-        const countryLanguages = document.createElement("p");
-        countryLanguages.innerText = `Languages: ${Object.values(country.languages).join(', ')}`;
-        countryContainer.appendChild(countryLanguages);
-
-        // Display population
-        const countryPopulation = document.createElement("p");
-        countryPopulation.innerText = `Population: ${country.population}`;
-        countryContainer.appendChild(countryPopulation);
-
-        // Append the country container to the document
+function displayCountries(countries) {
+    countries.forEach((country) => {
+        const countryContainer = createCountryContainer(country);
         document.getElementById("countries").appendChild(countryContainer);
     });
-
 }
-getCountryByName("cameroon");
+
+function createCountryContainer(country) {
+    const countryContainer = document.createElement("div");
+
+    const nameCountry = document.createElement("h2");
+    nameCountry.innerText = `Country: ${country.name.common}`;
+    countryContainer.appendChild(nameCountry);
+
+    const countryCapital = document.createElement("p");
+    countryCapital.innerText = `Capital: ${country.capital}`;
+    countryContainer.appendChild(countryCapital);
+
+    const countryLanguages = document.createElement("p");
+    countryLanguages.innerText = `Languages: ${Object.values(country.languages).join(', ')}`;
+    countryContainer.appendChild(countryLanguages);
+
+    const countryPopulation = document.createElement("p");
+    countryPopulation.innerText = `Population: ${country.population}`;
+    countryContainer.appendChild(countryPopulation);
+
+    return countryContainer;
+}
+
+function displayError(message) {
+    const errorContainer = document.createElement("div");
+    errorContainer.style.color = "red";
+    errorContainer.innerText = message;
+    document.getElementById("countries").appendChild(errorContainer);
+}
+
+// error should show
+// getCountryByName("Hawaii");
+
+//testing functions
+
+getCountryByName("Indonesia");
+// getAllCountries();
